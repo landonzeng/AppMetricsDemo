36054c40e7979b8cc5d5d25ce6743cb73de8a2cb
landon <landonzeng@qq.com>
2017-11-27 18:16:52 +0800

    first commit

---
+++ AppMetricsDemo/Demo/Startup.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using App.Metrics;
+
+namespace Demo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            #region Metrics监控配置
+            string IsOpen = Configuration.GetSection("InfluxDB")["IsOpen"].ToLower();
+            if (IsOpen == "true")
+            {
+                string database = Configuration.GetSection("InfluxDB")["DataBaseName"];
+                string InfluxDBConStr = Configuration.GetSection("InfluxDB")["ConnectionString"];
+                string app = Configuration.GetSection("InfluxDB")["app"];
+                string env = Configuration.GetSection("InfluxDB")["env"];
+                string username = Configuration.GetSection("InfluxDB")["username"];
+                string password = Configuration.GetSection("InfluxDB")["password"];
+
+                var uri = new Uri(InfluxDBConStr);
+
+                var metrics = AppMetrics.CreateDefaultBuilder()
+                .Configuration.Configure(
+                options =>
+                {
+                    options.AddAppTag(app);
+                    options.AddEnvTag(env);
+                })
+                .Report.ToInfluxDb(
+                options =>
+                {
+                    options.InfluxDb.BaseUri = uri;
+                    options.InfluxDb.Database = database;
+                    options.InfluxDb.UserName = username;
+                    options.InfluxDb.Password = password;
+                    options.HttpPolicy.BackoffPeriod = TimeSpan.FromSeconds(30);
+                    options.HttpPolicy.FailuresBeforeBackoff = 5;
+                    options.HttpPolicy.Timeout = TimeSpan.FromSeconds(10);
+                    options.FlushInterval = TimeSpan.FromSeconds(5);
+                })
+                .Build();
+
+                services.AddMetrics(metrics);
+                services.AddMetricsReportScheduler();
+                services.AddMetricsTrackingMiddleware();
+                services.AddMetricsEndpoints();
+
+            }
+            #endregion
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            #region 注入Metrics
+            string IsOpen = Configuration.GetSection("InfluxDB")["IsOpen"].ToLower();
+            if (IsOpen == "true")
+            {
+                app.UseMetricsAllMiddleware();
+                // Or to cherry-pick the tracking of interest
+                app.UseMetricsActiveRequestMiddleware();
+                app.UseMetricsErrorTrackingMiddleware();
+                app.UseMetricsPostAndPutSizeTrackingMiddleware();
+                app.UseMetricsRequestTrackingMiddleware();
+                app.UseMetricsOAuth2TrackingMiddleware();
+                app.UseMetricsApdexTrackingMiddleware();
+
+                app.UseMetricsAllEndpoints();
+                // Or to cherry-pick endpoint of interest
+                app.UseMetricsEndpoint();
+                app.UseMetricsTextEndpoint();
+                app.UseEnvInfoEndpoint();
+            }
+            #endregion
+
+            app.UseMvc();
+        }
+    }
+}
